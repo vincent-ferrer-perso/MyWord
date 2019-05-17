dea0645c31613e344cf447bf859d6a79ad550808
VINCENT <vincentdu13300@gmail.com>
2019-05-17 11:07:42 +0200

    Bouton menu fichier ok + debut raccourci

--- src/main/java/MyNotes.java
+++ src/main/java/MyNotes.java
@@ -12,6 +12,8 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -22,6 +24,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.lang.*;
 
@@ -32,6 +35,8 @@ import java.util.logging.Logger;
 
 import javafx.stage.DirectoryChooser;
 import sun.applet.Main;
+
+import javax.imageio.ImageIO;
 
 import static javafx.geometry.Pos.*;
 import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
@@ -50,6 +55,10 @@ public class MyNotes extends Application {
     private Menu menuPositionTexte;
     private Menu menuCopier;
 
+    private HBox commandsShortcut;
+    private Button saveShortcut;
+
+
     private MenuItem newFile;
     private MenuItem open;
     private MenuItem save;
@@ -62,6 +71,7 @@ public class MyNotes extends Application {
 
     private TextArea textArea;
 
+    private String currentWorkingDir;
     private FileReader fileRead;
     private FileWriter fileWriter;
     private FileChooser fileChooser;
@@ -77,7 +87,6 @@ public class MyNotes extends Application {
     private Desktop desktop;
 
     Optional<ButtonType> creerAlerteConfirmation(Alert nomAlerte, String titreAlerte, String action,String complement) {
-
         nomAlerte.setAlertType(CONFIRMATION);
         nomAlerte.setTitle(titreAlerte);
         nomAlerte.setHeaderText(null);
@@ -106,41 +115,64 @@ public class MyNotes extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        EventHandler<ActionEvent> ecouteurOpen = event -> {
-            fileChooser = new FileChooser();
-            File file = fileChooser.showOpenDialog(primaryStage);
-            BufferedReader br;
-            String ligneLu;
-            try{
-                fileRead = new FileReader(file);
-                br = new BufferedReader(fileRead);
-                while((ligneLu = br.readLine()) != null)
-                {
-                    textArea.appendText(ligneLu+'\n');
-                }
-                br.close();
-                primaryStage.setTitle(file.getName());
-            }catch(FileNotFoundException e){
-                e.printStackTrace();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        };
-
         EventHandler<ActionEvent> ecouteurSave = event -> {
-            fileChooser = new FileChooser();
-            File file = fileChooser.showSaveDialog(primaryStage);
-            try {
-                fileWriter = new FileWriter(file);
-                fileWriter.write(textArea.getText());
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (primaryStage.getTitle() == "MyNotes") {
+                fileChooser = new FileChooser();
+                File file = fileChooser.showSaveDialog(primaryStage);
+                currentWorkingDir = file.getParent();
+                try {
+                    fileWriter = new FileWriter(file);
+                    fileWriter.write(textArea.getText());
+                    fileWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                primaryStage.setTitle(file.getName());
+            }else{
+                try {
+
+                    FileWriter fw = new FileWriter(currentWorkingDir+"/"+primaryStage.getTitle());
+                    fw.write(textArea.getText());
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
+        };//ecouteurSave
 
-        };
+        EventHandler<ActionEvent> ecouteurOpen = event -> {
+            Alert alertOuvrirSansSauvegarder =new Alert(CONFIRMATION);
+            Optional<ButtonType> rep;
+            rep =  creerAlerteConfirmation(alertOuvrirSansSauvegarder,
+                    "Ouvrir un autre fichier",
+                    "sauvegarder",
+                    "avant d'ouvrir un autre fichier");
+            if (rep.get() == ButtonType.OK) {
+                ecouteurSave.handle(event);
+                textArea.clear();
+                fileChooser = new FileChooser();
+                File file = fileChooser.showOpenDialog(primaryStage);
+                BufferedReader br;
+                String ligneLu;
+                try {
+                    fileRead = new FileReader(file);
+                    br = new BufferedReader(fileRead);
+                    currentWorkingDir = file.getParent();
+                    while ((ligneLu = br.readLine()) != null) {
+                        textArea.appendText(ligneLu + '\n');
+                    }
+                    br.close();
+                    primaryStage.setTitle(file.getName());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };//ecouteurOpen
 
-        EventHandler<ActionEvent> ecouteurNewFile = event -> {//Nouveau fichier ouvert mais ancien pas save
+        EventHandler<ActionEvent> ecouteurNewFile = event -> {
             Alert alertOuvrirSansSauvegarder =new Alert(CONFIRMATION);
             Optional<ButtonType> rep;
             rep =  creerAlerteConfirmation(alertOuvrirSansSauvegarder,
@@ -148,9 +180,10 @@ public class MyNotes extends Application {
                                       "sauvegarder",
                                  "avant de créer un nouveau fichier");
             if (rep.get() == ButtonType.OK){
+                ecouteurSave.handle(event);
             }
             start(primaryStage);
-        };
+        };//ecouteurSaveNewFile
 
 
         newFile = new MenuItem("Nouveau");
@@ -165,6 +198,8 @@ public class MyNotes extends Application {
         cut    = new MenuItem("Couper");
         paste  = new MenuItem("Coller");
 
+        saveShortcut = new Button();
+
         menuFile = new Menu("Fichier");
         menuFile.getItems().addAll(newFile,open,save);
         menuPositionTexte = new Menu("Position");
@@ -174,13 +209,20 @@ public class MyNotes extends Application {
 
         menuBar = new MenuBar(menuFile,menuPositionTexte,menuCopier);
 
+
+        saveShortcut.setStyle("-fx-background-image: disquette'.'png;");//pas image
+
+
+        commandsShortcut = new HBox(saveShortcut);
+
+
         textArea = new TextArea();
         textArea.setWrapText(true);
         textArea.setPrefRowCount(150);
 
 
 
-        root = new VBox(menuBar,textArea);
+        root = new VBox(menuBar,commandsShortcut,textArea);
         root.setFillWidth(true);
 
         scene = new Scene(root);
@@ -197,12 +239,17 @@ public class MyNotes extends Application {
         open.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
 
         save.setOnAction(ecouteurSave);
+        saveShortcut.setOnAction(ecouteurSave);
         save.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
 
         center.setOnAction(ecouteurCentrer);
+        center.setAccelerator(KeyCombination.keyCombination("Ctrl+E"));
         copy.setOnAction(ecouteurCopy);
+        copy.setAccelerator(KeyCombination.keyCombination("Ctrl+C"));
         cut.setOnAction(ecouteurCut);
+        cut.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
         paste.setOnAction(ecouteurPaste);
+        paste.setAccelerator(KeyCombination.keyCombination("Ctrl+V"));
 
 
 
